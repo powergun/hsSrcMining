c63a35d1f6cf0fb6d536627141c4289fccd62407
Wei Ning <macgnw@gmail.com>
2020-01-05 20:57:52 +1100

    init sub project git fu; gitrev demo

---
+++ gitfu/README.md
@@ -0,0 +1,9 @@
+# Git Fu
+
+## Git-Revision
+
+useful for embedding git hash in `--version` or panic message;
+
+source: <https://github.com/acfoltzer/gitrev/blob/master/Example.hs>
+
+mentioned in First Principles as a demo for applicative
---
+++ gitfu/app/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell   #-}
+module Main where
+import           Development.GitRev
+import           Prelude            (String, concat, error, otherwise)
+
+panic :: String -> a
+panic msg = error panicMsg
+  where panicMsg =
+          concat [ "[panic ", $(gitBranch), "@", $(gitHash)
+                 , " (", $(gitCommitDate), ")"
+                 , " (", $(gitCommitCount), " commits in HEAD)"
+                 , dirty, "] ", msg ]
+        dirty | $(gitDirty) = " (uncommitted files present)"
+              | otherwise   = ""
+
+main = panic "oh no!"
